b52cedc9eb16a9e0f29c200e99d39907c27a2b12
Mark Pitman <marktpitman@gmail.com>
2016-12-02 11:00:13 -0500

    added basic models, moved parseService to services.js

--- public/javascripts/models.js
+++ public/javascripts/models.js
@@ -1,5 +1,120 @@
 (function() {
     'use strict';
 
-    angular.module('LazMovieReviews');
+    /**
+     * Models
+     */
+    angular.module('LazMovieReviews')
+
+    .factory('User', [
+        '$q',
+        function ($q) {
+            function User(data) {
+                if (data) {
+                    this.setData(data);
+                }
+            }
+
+            User.prototype = {
+                setData: function(data) {
+                    angular.extend(this, data);
+                },
+                load: function(id) {
+                    console.debug('TODO: load User data');
+                },
+                save: function() {
+                    console.debug('TODO: save User data');
+                },
+                delete: function() {
+                    console.debug('TODO: delete User data');
+                }
+            };
+
+            return User;
+        }
+    ])
+
+    .factory('Movie', [
+        '$q',
+        function ($q) {
+            function Movie(data) {
+                if (data) {
+                    this.setData(data);
+                }
+            }
+
+            Movie.prototype = {
+                setData: function(data) {
+                    angular.extend(this, data);
+                },
+                load: function(id) {
+                    console.debug('TODO: load User data');
+                },
+                save: function() {
+                    console.debug('TODO: save User data');
+                },
+                delete: function() {
+                    console.debug('TODO: delete User data');
+                }
+            };
+
+            return Movie;
+        }
+    ])
+
+    .factory('Review', [
+        '$q',
+        function ($q) {
+            function Review(data) {
+                if (data) {
+                    this.setData(data);
+                }
+            }
+
+            Review.prototype = {
+                setData: function(data) {
+                    angular.extend(this, data);
+                },
+                load: function(id) {
+                    console.debug('TODO: load User data');
+                },
+                save: function() {
+                    console.debug('TODO: save User data');
+                },
+                delete: function() {
+                    console.debug('TODO: delete User data');
+                }
+            };
+
+            return Review;
+        }
+    ])
+
+    .factory('Comment', [
+        '$q',
+        function ($q) {
+            function Comment(data) {
+                if (data) {
+                    this.setData(data);
+                }
+            }
+
+            Comment.prototype = {
+                setData: function(data) {
+                    angular.extend(this, data);
+                },
+                load: function(id) {
+                    console.debug('TODO: load User data');
+                },
+                save: function() {
+                    console.debug('TODO: save User data');
+                },
+                delete: function() {
+                    console.debug('TODO: delete User data');
+                }
+            };
+
+            return Comment;
+        }
+    ]);
 })();
