68c54d73c921012273bd76be318e876820dd2725
Mark Pitman <marktpitman@gmail.com>
2016-12-02 11:00:57 -0500

    removed parseService from lazMovieApp.js

--- public/javascripts/lazMovieApp.js
+++ public/javascripts/lazMovieApp.js
@@ -20,35 +20,6 @@
         );
     }
 
-    var parseService = function($http){
-        var url = 'https://lazmoviereviews.herokuapp.com/parse/classes/Users';
-        var config = {headers: { 'X-Parse-Application-Id':'9d300721-df9d-42bc-8411-1659efbbed66'}};
-
-        function addUser(data){
-             return $http.post(url, data, config)
-                .then(function success(response){
-                    return data;
-                }, function fail(response){
-                    console.log(JSON.stringify(response));
-                });
-        }
-
-        function getUsers(){
-            return $http.get(url, config)
-                .then(function success(response){
-                    return response.data.results;
-                }, function fail(response){
-                    console.log(JSON.stringify(response));
-                });
-        }
-
-        return {
-            addUser: addUser,
-            getUsers: getUsers
-        };
-    }
-    app.factory('parseService', parseService);
-
     app.component('proofOfConcept', {
         templateUrl: 'public/templates/proofOfConcept.html',
         controller: ProofOfConceptController
