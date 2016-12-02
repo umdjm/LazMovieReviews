74fb6ef8e22cec76013a176d6dd04c59a163fbc9
mkouba <michelle.kouba@learninga-z.com>
2016-12-02 12:01:22 -0500

    saving service and removing bad merge comments

--- public/javascripts/lazMovieApp.js
+++ public/javascripts/lazMovieApp.js
@@ -1,20 +1,7 @@
 (function(){
-<<<<<<< HEAD
     var app = angular.module('LazMovieReviews', [
         'ngRoute'
     ]);
-=======
-    var app = angular.module('LazMovieReviews', ['ngResource']).controller('MainCtrl', function MainCtrl() {});
-
-    function ProofOfConceptController(parseService) {
-        var self = this;
-        self.addItem = function(){
-            parseService.addUser(self.newUser)
-                .then(function(user){
-                    self.users.push(user);
-                    self.newUser = {};
-                })
->>>>>>> 811e9e73e3929c20d149c1c0661819bb962f78dc
 
     app.config([
         '$locationProvider',
