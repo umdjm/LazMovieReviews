42d01c2e0d6a81b99cb392630f6722d4231b6e9c
DJ Martin <umdjm@hotmail.com>
2016-12-02 00:34:18 -0500

    Parse Proof of Concept

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ var path = require('path');
 var app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(__dirname + '/public'));
-
-app.get('/*', function(req, res){
+app.use(express.static('public'))
+app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
---
+++ public/javascripts/lazMovieApp.js
@@ -0,0 +1,56 @@
+
+(function(){
+    var app = angular.module('LazMovieReviews', []).controller('MainCtrl', function MainCtrl() {});
+
+    function ProofOfConceptController(parseService) {
+        var self = this;
+        self.addItem = function(){
+            parseService.addUser(self.newUser)
+                .then(function(user){
+                    self.users.push(user);
+                    self.newUser = {};
+                })
+
+            return;
+        };
+        parseService.getUsers().then(
+            function(users){
+                self.users = users;
+            }
+        );
+    }
+
+    var parseService = function($http){
+        var url = 'https://lazmoviereviews.herokuapp.com/parse/classes/Users';
+        var config = {headers: { 'X-Parse-Application-Id':'9d300721-df9d-42bc-8411-1659efbbed66'}};
+
+        function addUser(data){
+             return $http.post(url, data, config)
+                .then(function success(response){
+                    return data;
+                }, function fail(response){
+                    console.log(JSON.stringify(response));
+                });
+        }
+
+        function getUsers(){
+            return $http.get(url, config)
+                .then(function success(response){
+                    return response.data.results;
+                }, function fail(response){
+                    console.log(JSON.stringify(response));
+                });
+        }
+
+        return {
+            addUser: addUser,
+            getUsers: getUsers
+        };
+    }
+    app.factory('parseService', parseService);
+
+    app.component('proofOfConcept', {
+        templateUrl: '/templates/proofOfConcept.html',
+        controller: ProofOfConceptController
+    });
+}())
