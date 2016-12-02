d721de13648309c967f693b6d481221e09b70648
DJ Martin <umdjm@hotmail.com>
2016-12-01 22:55:16 -0500

    simplify node

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
-var index = require('./routes/index');
 var app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', index);
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
 module.exports = app;
