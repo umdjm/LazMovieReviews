db2c5a7622819613d5c69e4e4fd7691349dafe6a
Michelle Kouba <mkouba@CLG.Local>
2016-12-02 11:55:52 -0500

    Merge branch 'master' of C:\hackathons\LazMovieReviews with conflicts.

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,44 +1,68 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('LazMovieReviews')
 
-        .factory('parseService', [
-            '$http',
-            function ($http) {
-                var url = 'https://lazmoviereviews.herokuapp.com/parse/classes/Users';
-                var config = {headers: {'X-Parse-Application-Id': '9d300721-df9d-42bc-8411-1659efbbed66'}};
+    .factory('parseService', [
+        '$http',
+        function($http) {
+            var url = 'https://lazmoviereviews.herokuapp.com/parse/classes/Users';
+            var config = {headers: { 'X-Parse-Application-Id':'9d300721-df9d-42bc-8411-1659efbbed66'}};
 
-                function addUser(data) {
-                    return $http.post(url, data, config)
-                        .then(function success(response) {
-                            return data;
-                        }, function fail(response) {
-                            console.log(JSON.stringify(response));
-                        });
-                }
-
-                function getUsers() {
-                    return $http.get(url, config)
-                        .then(function success(response) {
-                            return response.data.results;
-                        }, function fail(response) {
-                            console.log(JSON.stringify(response));
-                        });
-                }
-
-                return {
-                    addUser: addUser,
-                    getUsers: getUsers
-                };
+            function addUser(data) {
+                return $http.post(url, data, config)
+                    .then(function success(response) {
+                        return data;
+                    }, function fail(response) {
+                        console.log(JSON.stringify(response));
+                    });
             }
-        ])
 
-        .factory('Omdb', [
-            '$resource',
-            function ($resource) {
-
+            function getUsers() {
+                return $http.get(url, config)
+                    .then(function success(response) {
+                        return response.data.results;
+                    }, function fail(response){
+                        console.log(JSON.stringify(response));
+                    });
             }
-        ]);
+
+            return {
+                addUser: addUser,
+                getUsers: getUsers
+            };
+        }
+    ])
+
+    .factory('reviewService', [
+        '$http',
+        function($http) {
+            var url = 'https://lazmoviereviews.herokuapp.com/parse/classes/Reviews';
+            var config = {headers: { 'X-Parse-Application-Id':'9d300721-df9d-42bc-8411-1659efbbed66'}};
+
+            function addReview(data) {
+                return $http.post(url, data, config)
+                    .then(function success(response) {
+                        return data;
+                    }, function fail(response) {
+                        console.log(JSON.stringify(response));
+                    });
+            }
+
+            function getReview() {
+                return $http.get(url, config)
+                    .then(function success(response) {
+                        return response.data.results;
+                    }, function fail(response){
+                        console.log(JSON.stringify(response));
+                    });
+            }
+
+            return {
+                addReview: addReview,
+                getReview: getReview
+            };
+        }
+    ]);
 
 })();
